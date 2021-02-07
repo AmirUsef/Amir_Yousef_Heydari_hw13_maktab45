21f72d6310780758120ec760a0999d8c8cd9e7f5
Amir Yousef Heydari <ayh1380@gmail.com>
2021-02-08 00:58:02 +0330

    q6 handled with req.param

--- 6/routes/pages.js
+++ 6/routes/pages.js
@@ -2,8 +2,15 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-router.get(/^\/index[1-5].html$/, (req, res) => {
-    res.sendFile(path.join(__dirname, `../public${req.url}`));
+// router.get(/^\/index[1-5].html$/, (req, res) => {
+//     res.sendFile(path.join(__dirname, `../public${req.url}`));
+// });
+
+router.get('/:pageNum', (req, res) => {
+    if (req.params.pageNum > 0 && req.params.pageNum < 6)
+        res.sendFile(path.join(__dirname, `../public/index${req.params.pageNum}.html`));
+    else
+        res.send('404 page not found')
 });
 
 module.exports = router;
